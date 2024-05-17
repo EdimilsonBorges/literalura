e021007d7d0cdfa797c56379080557ac9198db85
EdimilsonBorges <edimilson-borges159@hotmail.com>
2024-05-16 22:40:27 -0300

    feat: commit inicial

---
+++ src/main/java/com/edimilson/literalura_app/LiteraluraAppApplication.java
@@ -0,0 +1,13 @@
+package com.edimilson.literalura_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteraluraAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiteraluraAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edimilson/literalura_app/LiteraluraAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edimilson.literalura_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteraluraAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
