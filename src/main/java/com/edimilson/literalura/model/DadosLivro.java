6827c798d8fd5bd142b5d2a555da64666414bb1a
EdimilsonBorges <edimilson-borges159@hotmail.com>
2024-05-23 11:12:43 -0300

    feat: implementado buscar lista pelo titulo

--- src/main/java/com/edimilson/literalura/model/DadosLivro.java
+++ src/main/java/com/edimilson/literalura/model/DadosLivro.java
@@ -6,7 +6,8 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record DadosLivro(@JsonAlias("title") String titulo,
+public record DadosLivro(Integer id,
+                         @JsonAlias("title") String titulo,
                          @JsonAlias("languages") List<String> idioma,
                          @JsonAlias("authors") List<DadosAutor> autor,
                          @JsonAlias("download_count") Integer numeroDownloads) {
--- src/main/java/com/edimilson/literalura/principal/Principal.java
+++ src/main/java/com/edimilson/literalura/principal/Principal.java
@@ -1,10 +1,20 @@
 package com.edimilson.literalura.principal;
 
+import com.edimilson.literalura.model.DadosApi;
+import com.edimilson.literalura.model.DadosLivro;
+import com.edimilson.literalura.service.ConsumoApi;
+import com.edimilson.literalura.service.ConverteDados;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Principal {
-    private Scanner leitura = new Scanner(System.in);
-    private final String URL_BASE = "https://gutendex.com/books/?search=";
+    private final String URL_BASE = "https://gutendex.com/books/";
+    private final Scanner leitura = new Scanner(System.in);
+    private final ConsumoApi consumoApi = new ConsumoApi();
+    private final ConverteDados conversor = new ConverteDados();
+    private final List<DadosLivro> livros = new ArrayList<>();
 
     public void exibeMenu(){
 
@@ -18,20 +28,22 @@ public class Principal {
                 2) Listar livos registrados
                 3) Listar autores registrados
                 4) Listar autores vivos em um determinado ano
-                5) Listar livros em um determinado ano
+                5) Listar livros em um determinado idioma
                 0) Sair
                 -----------------------------------------------
                 """;
             System.out.println(menu);
-            String opçao = leitura.nextLine();
+            String opcao = leitura.nextLine();
 
-            switch (opçao){
+            switch (opcao){
                 case "0":
                     sair = true;
                     System.out.println("Você saiu.");
                     break;
                 case "1":
                     System.out.println("Insira o nome do livro que você deseja procurar");
+                    String nome = leitura.nextLine();
+                    buscarLivro(nome);
                     break;
                 case "2":
                     break;
@@ -55,5 +67,63 @@ public class Principal {
                     System.out.println("Opção inválida digite de 0 a 5");
             }
         }
+    }
+
+    private void buscarLivro(String nome) {
+        System.out.println("Buscando, aguarde...");
+		String json = consumoApi.obterDados(URL_BASE + "?search=" + nome.replace(" ", "%20"));
+		DadosApi dadosApi = conversor.obterDados(json, DadosApi.class);
+
+        if(dadosApi.quantidade() == 0){
+            System.out.println("Não foram encontrado livros nessa pesquisa");
+        }else if(dadosApi.quantidade() == 1){
+            obterLivro(dadosApi, 0);
+        }else {
+            System.out.println("Foram encontrado " + dadosApi.quantidade() + " livros nessa pesquisa, deseja ver todos esses livros? (S/N)");
+            String opcao = leitura.nextLine();
+            if(opcao.equalsIgnoreCase("s")){
+                percorrerLivros(dadosApi);
+            }
+        }
+    }
+
+    private void percorrerLivros(DadosApi dadosApi){
+        int proximo = dadosApi.livros().size();
+        String json;
+        int index = 0;
+
+        for(int i = 1; i <= dadosApi.quantidade(); i++){
+            if( i <= proximo ){
+                obterLivro(dadosApi, index);
+                index++;
+            }else{
+                proximo += dadosApi.livros().size();
+                json = consumoApi.obterDados(dadosApi.proximo());
+                dadosApi = conversor.obterDados(json, DadosApi.class);
+                index = 0;
+                obterLivro(dadosApi, index);
+                index++;
+            }
+        }
+    }
+
+    private void obterLivro(DadosApi dadosApi, int index) {
+        String json = consumoApi.obterDados(URL_BASE + dadosApi.livros().get(index).id() + "/");
+        DadosLivro dadosLivro = conversor.obterDados(json, DadosLivro.class);
+        livros.add(dadosLivro);
+        imprimirLivro(dadosLivro);
+    }
+
+    private void imprimirLivro(DadosLivro dadosLivro){
+        String autor = !dadosLivro.autor().isEmpty() ? String.valueOf(dadosLivro.autor().getFirst().nome()) : "Autor desconhecido";
+        String livro = String.format("""
+                --------------------------------
+                Titulo: %s
+                Autor: %s
+                Idioma: %s
+                Número de Downloads: %s
+                --------------------------------
+                """, dadosLivro.titulo(), autor, dadosLivro.idioma().getFirst(),dadosLivro.numeroDownloads());
+        System.out.println(livro);
     }
 }
